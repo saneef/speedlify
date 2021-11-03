9b6e9a3fd8ed54e94b95dd3cb627b093e0060613
Saneef Ansari <hello@saneef.com>
2021-11-03 12:56:01 +0530

    reduces frequency to 1 min

--- _data/sites/website.js
+++ _data/sites/website.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "websites", // optional, falls back to object key
 	description: "Tracking Websites",
 	options: {
-		frequency: 60 * 48, // (in minutes), 23 hours
+		frequency: 1, // (in minutes)
 	},
 	urls: ["https://saneef.com/"],
 };
