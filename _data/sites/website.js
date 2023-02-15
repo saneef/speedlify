7ee71f64b6820958251c512112490373fd78ff0d
Saneef Ansari <hello@saneef.com>
2023-02-15 23:27:48 +0530

    Adds unravelurl.com

--- _data/sites/website.js
+++ _data/sites/website.js
@@ -1,8 +1,8 @@
 module.exports = {
-	name: "websites", // optional, falls back to object key
-	description: "Tracking Websites",
-	options: {
-		frequency: 1, // (in minutes)
-	},
-	urls: ["https://saneef.com/"],
+  name: "websites", // optional, falls back to object key
+  description: "Tracking Websites",
+  options: {
+    frequency: 1, // (in minutes)
+  },
+  urls: ["https://saneef.com/", "https://unravelurl.com"],
 };
